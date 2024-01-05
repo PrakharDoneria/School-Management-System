6b9c32e3803bf372d9dc498f2399c249b31453cb
Prakhar Doneria <gamesprotec@gmail.com>
2024-01-05 19:16:02 +0530

    Configuration

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+# MySQL Database Configuration
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'prakjardoneria',
+    'password': 'Kyu_batau?',
+    'database': 'school_management'
+}
